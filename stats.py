9cf1b872f5fe9bfce6cd5bffbee50ae5cf0a2e1e
Aarav Khandelwal <khandekaarav58@gmail.com>
2026-07-02 14:39:38 +0530

    stats day 1 — mean, median, std dev, variance

---
+++ stats.py
@@ -0,0 +1,9 @@
+import numpy as np
+data= [23,45,67,89,34,56,78,90,11]
+print("mean",np.mean(data))
+print("Median",np.median(data))
+print("std dev",np.std(data))
+print("variance",np.var(data))
+print("min",np.min(data))
+
+print("max",np.max(data))
